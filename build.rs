f34c6173496abea33ae81154afa2cf14f7265ac8
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2026-07-16 00:15:42 +0530

    Bump pyo3 to 0.29

--- build.rs
+++ build.rs
@@ -12,21 +12,15 @@ fn main() {
         println!("{line}");
     }
 
-    if !config.suppress_build_script_link_lines {
-        if let Some(lib_dir) = &config.lib_dir {
+    if std::env::var_os("PYO3_BUILD_EXTENSION_MODULE").is_none()
+        && !config.suppress_build_script_link_lines()
+    {
+        if let Some(lib_dir) = config.lib_dir() {
             println!("cargo:rustc-link-search=native={}", lib_dir);
         }
-        if let Some(lib_name) = &config.lib_name {
+        if let Some(lib_name) = config.lib_name() {
             println!("cargo:rustc-link-lib={}", lib_name);
         }
-    }
-
-    for extra in &config.extra_build_script_lines {
-        println!("{extra}");
-    }
-
-    if let Some(prefix) = &config.python_framework_prefix {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,{prefix}");
     }
 
     pyo3_build_config::add_extension_module_link_args();
